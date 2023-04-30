ceec4a6f518c8ae468a073d29dc6b2b5e4905b30
Anna Cole <acrrj123@gmail.com>
2023-04-29 19:43:30 -0500

    Final changes.

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ function findMatching(array, name) {
 }
 
 //The filter() method creates a new array, which we can return directly. Inside the parenthesis must be
-//the callback function, which takes elements as paramether and add the elements that satisfies the 
+//the callback function, which takes element as paramether and add the elements that satisfies the 
 //conditon to the return of the filter() (i.e, a new array).  
 
-//console.log(findMatching(drivers, "Bobby"))
+console.log(findMatching(drivers, "bobby"))
 
 //This function takes an array of drivers' names and a string as arguments for querying the array, 
 //and returns all drivers whose names begin with the provided letters.
 
 function fuzzyMatch(array, letter) {
-    return array.filter(element => element.startsWith(letter))
+    return array.filter(element => element.toUpperCase().startsWith(letter.toUpperCase()))
 }
-//console.log(fuzzyMatch(drivers, "A"))
+console.log(fuzzyMatch(drivers, "a"))
 
 //This function takes an array of driver objects and a string as arguments. 
 //Each driver object has two properties: name and hometown. The function should return
@@ -30,6 +30,6 @@ const driversObj = [{name: "Ana", hometown: "Niteroi"}, {name: "Stuart", hometow
 function matchName(array, name) {
     return array.filter(element => element.name === name)
 }
-//console.log(matchName(driversObj, "Ana"))
+console.log(matchName(driversObj, "Ana"))
 
 
